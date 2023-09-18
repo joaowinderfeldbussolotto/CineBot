af862bbe5af4004da3c097f00f8a2e2d10470fd7
joaowinderfeldbussolotto <joaowinderfeldbussolotto@gmail.com>
2023-09-17 22:44:29 -0300

    :bug: fix: altering reservation id type

--- bot-backend/models/Reservation.py
+++ bot-backend/models/Reservation.py
@@ -6,7 +6,7 @@ from models.Session import Session
 class Reservation(Base):
     __tablename__ = 'reservas'
 
-    id = Column(Integer, primary_key=True)
+    id = Column(String(40), primary_key=True)
     session_id = Column("id_sessao", ForeignKey('sessoes.id'), nullable=False, index=True)
     user_name = Column("nome_usuario", String(255), nullable=False)
     user_email = Column("email_usuario", String(255), nullable=False)
